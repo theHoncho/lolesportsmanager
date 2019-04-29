13fe1a0c94b6ea53a423260c31927231dd5a16d5
Matthew Overton <matthewloverton@gmail.com>
2019-04-29 23:36:54 +0100

    Changed navigation

--- lolesportsmanager/Startup.cs
+++ lolesportsmanager/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace lolesportsmanager
 {
